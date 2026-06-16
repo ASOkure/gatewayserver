64bee26c5485b9043353bc837eb3ab1287a0c514
akanimo sam okure <akan.okure@gmail.com>
2026-06-16 23:09:27 +0100

    setup

---
+++ src/test/java/com/mantletechsolutions/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mantletechsolutions.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
